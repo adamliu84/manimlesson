babbcf1f0b05c15c081efe751c591225a74e8cdb
adamliu84 <adam.liu84@gmail.com>
2024-04-27 22:14:50 +0800

    Adding split arrow and payout anim

--- TestDest.py
+++ TestDest.py
@@ -20,16 +20,38 @@ class Dest(Scene):
         # Application
         a_num = Tex("20").shift(RIGHT*2).shift(DOWN*0.05)
         m_num = Tex("80").shift(RIGHT*3).shift(DOWN*0.05)
-        self.play(Write(a_num), Write(m_num), FadeOut(c_num))
+        ca_arrow = Line(start=c_num.get_bottom(),
+                        end=a_num.get_top()).add_tip()
+        cm_arrow = Line(start=c_num.get_bottom(),
+                        end=m_num.get_top()).add_tip()
+        self.play(FadeIn(ca_arrow, cm_arrow))
+        self.play(Write(a_num), Write(m_num), FadeOut(
+            c_num), FadeOut(ca_arrow), FadeOut(cm_arrow))
         self.wait()
         self.play(a_num.animate.shift(LEFT*4.5),
                   m_num.animate.shift(LEFT*0.5),
                   run_time=1)
 
         # Fee
-        s_num = Tex("5").move_to([a_num.get_x()-0.5, a_num.get_y()-0.5, 0])
-        p_num = Tex("15").move_to([a_num.get_x()+0.5, a_num.get_y()-0.5, 0])
-        self.play(Write(s_num), Write(p_num), FadeOut(a_num))
+        s_num = Tex("5").move_to([a_num.get_x()-0.5, a_num.get_y()-1, 0])
+        p_num = Tex("15").move_to([a_num.get_x()+0.5, a_num.get_y()-1, 0])
+        as_arrow = Line(start=a_num.get_bottom(),
+                        end=s_num.get_top()).add_tip()
+        ap_arrow = Line(start=a_num.get_bottom(),
+                        end=p_num.get_top()).add_tip()
+        self.play(FadeIn(as_arrow, ap_arrow))
+        self.play(Write(s_num), Write(p_num), FadeOut(
+            a_num), FadeOut(as_arrow), FadeOut(ap_arrow))
         self.wait()
-        self.play(s_num.animate.to_edge(LEFT, buff=0.5),
-                  p_num.animate.shift(LEFT*0.5))
+        self.play(
+            # s_num.animate.to_edge(LEFT, buff=0.5),
+            # s_num.animate.next_to(prect, LEFT, buff=0.2),
+            # s_num.animate.shift([-2, 0, 0])
+            s_num.animate.shift(LEFT*2),
+            p_num.animate.shift(LEFT*0.5)
+        )
+
+        # Payout
+        self.play(m_num.animate.next_to(crect, DOWN, buff=0.2),
+                  p_num.animate.next_to(prect, DOWN, buff=0.2))
+        self.wait(2)
