b19569c9095fdbbd1110fb29a94cd5ca8a230612
adamliu84 <adam.liu84@gmail.com>
2024-05-02 00:01:39 +0800

    Apply function brief understanding

--- devtaosim101/BasicElement.py
+++ devtaosim101/BasicElement.py
@@ -16,3 +16,26 @@ class UpdaterExample(Scene):
         text.add_updater(textUpdater)
         self.add(text)
         self.wait(10)
+
+class ApplyFunctionExample(Scene):
+    def construct(self):
+        text = Tex("Text")\
+               .to_corner(DL)
+
+        self.add(text)
+
+        def apply_function(mob):
+            mob.scale(2)
+            mob.to_corner(UR)
+            mob.rotate(PI/4)
+            mob.set_color(RED)
+            return mob
+
+        self.play(
+            ApplyFunction(
+                apply_function,
+                text
+            )
+        )
+
+        self.wait(0.3)
