ecbea3cbe72fc578fdf52e6c0ec29416b4870b1d
adamliu84 <adam.liu84@gmail.com>
2024-06-26 21:00:19 +0800

    Learning about angle (initial version)

--- devtaosim101/BasicUpdaters.py
+++ devtaosim101/BasicUpdaters.py
@@ -303,3 +303,101 @@ class MovingAngle(Scene):
         self.play(theta_tracker.animate.increment_value(140))
         self.play(tex.animate.set_color(RED), run_time=0.5)
         self.play(theta_tracker.animate.set_value(350))
+
+
+class AnglesExample(Scene):
+    def construct(self):
+        # line1 = Line(LEFT + (1/3) * UP, RIGHT + (1/3) * DOWN)
+        # line2 = Line(DOWN + (1/3) * RIGHT, UP + (1/3) * LEFT)
+        # angles = [
+        #     Angle(line1, line2),
+        #     Angle(line1, line2, radius=0.4, quadrant=(1, -1), other_angle=True),
+        #     Angle(line1, line2, radius=0.5, quadrant=(-1, 1),
+        #           stroke_width=8, other_angle=True),
+        #     Angle(line1, line2, radius=0.7, quadrant=(-1, -1), color=RED),
+        #     Angle(line1, line2, other_angle=True),
+        #     Angle(line1, line2, radius=0.4, quadrant=(1, -1)),
+        #     Angle(line1, line2, radius=0.5, quadrant=(-1, 1), stroke_width=8),
+        #     Angle(line1, line2, radius=0.7, quadrant=(-1, -1),
+        #           color=RED, other_angle=True),
+        # ]
+        # plots = VGroup()
+        # for angle in angles:
+        #     plot = VGroup(line1.copy(), line2.copy(), angle)
+        #     plots.add(VGroup(plot, SurroundingRectangle(plot, buff=0.3)))
+        # plots.arrange_in_grid(rows=2, buff=1)
+        # self.add(plots)
+        # self.wait()
+
+        angleTracker = DecimalNumber(0, num_decimal_places=0).to_corner(UR)
+        self.add(angleTracker)
+
+        line1 = Line(LEFT + (1/3) * UP, RIGHT + (1/3) * DOWN)
+        line2 = Line(DOWN + (1/3) * RIGHT, UP + (1/3) * LEFT)
+        line2.rotate_about_origin(angleTracker.get_value()*DEGREES)
+        angles = [
+            Angle(line1, line2),
+            Angle(line1, line2, radius=0.4, quadrant=(1, -1), other_angle=True),
+            Angle(line1, line2, radius=0.5, quadrant=(-1, 1),
+                  stroke_width=8, other_angle=True),
+            Angle(line1, line2, radius=0.7, quadrant=(-1, -1), color=RED),
+            Angle(line1, line2, other_angle=True),
+            Angle(line1, line2, radius=0.4, quadrant=(1, -1)),
+            Angle(line1, line2, radius=0.5, quadrant=(-1, 1), stroke_width=8),
+            Angle(line1, line2, radius=0.7, quadrant=(-1, -1),
+                  color=RED, other_angle=True),
+        ]
+        plots = VGroup()
+        for angle in angles:
+            invRect = Rectangle(height=2.5, width=2.5)
+            plot = VGroup(line1.copy(), line2.copy(), angle, invRect)
+            plots.add(VGroup(plot, SurroundingRectangle(plot, buff=0.3)))
+        plots.arrange_in_grid(rows=2, buff=1)
+
+        def plots_updater(mob):
+            line1 = Line(LEFT + (1/3) * UP, RIGHT + (1/3) * DOWN)
+            line2 = Line(DOWN + (1/3) * RIGHT, UP + (1/3) * LEFT)
+            line2.rotate_about_origin(angleTracker.get_value()*DEGREES)
+            angles = [
+                Angle(line1, line2),
+                Angle(line1, line2, radius=0.4,
+                      quadrant=(1, -1), other_angle=True),
+                Angle(line1, line2, radius=0.5, quadrant=(-1, 1),
+                      stroke_width=8, other_angle=True),
+                Angle(line1, line2, radius=0.7, quadrant=(-1, -1), color=RED),
+                Angle(line1, line2, other_angle=True),
+                Angle(line1, line2, radius=0.4, quadrant=(1, -1)),
+                Angle(line1, line2, radius=0.5,
+                      quadrant=(-1, 1), stroke_width=8),
+                Angle(line1, line2, radius=0.7, quadrant=(-1, -1),
+                      color=RED, other_angle=True),
+            ]
+            plots = VGroup()
+            for angle in angles:
+                invRect = Rectangle(height=2.5, width=2.5)
+                plot = VGroup(line1.copy(), line2.copy(), angle, invRect)
+                plots.add(VGroup(plot, SurroundingRectangle(plot, buff=0)))
+            plots.arrange_in_grid(rows=2, buff=1)
+            mob.become(plots)
+        plots.add_updater(plots_updater)
+        self.add(plots)
+        self.play(
+            ChangeDecimalToValue(angleTracker, 90),
+            run_time=2
+        )
+        self.wait()
+        self.play(
+            ChangeDecimalToValue(angleTracker, 180),
+            run_time=2
+        )
+        self.wait()
+        self.play(
+            ChangeDecimalToValue(angleTracker, 350),
+            run_time=2
+        )
+        self.wait()
+        self.play(
+            ChangeDecimalToValue(angleTracker, 0),
+            run_time=2
+        )
+        self.wait()
