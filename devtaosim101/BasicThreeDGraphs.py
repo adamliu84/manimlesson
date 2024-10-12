48361da2bb76a7a202362a980a2ff95336296a7d
adamliu84 <adam.liu84@gmail.com>
2024-10-12 16:49:35 +0800

    3D Surface learning

--- devtaosim101/BasicThreeDGraphs.py
+++ devtaosim101/BasicThreeDGraphs.py
@@ -101,3 +101,85 @@ class ThreeDLinesAndParamFuncExample(ThreeDScene):
     self.wait(2)
     self.move_camera(phi=0,run_time=1,rate_func=rush_from)
     self.wait(2)
+
+class ThreeDSurfaceExample(ThreeDScene):
+  def construct(self):
+    axes_3d = ThreeDAxes(
+        x_range=(-6,6,1),
+        x_length=12,
+        y_range=(-5,5,1),
+        y_length=10,
+        z_range=(-3,3,1),
+        z_length=6,
+    )
+    self.set_camera_orientation(phi=70*DEGREES,theta=240*DEGREES)
+
+    surface = Surface(
+        lambda u, v: np.array([
+            np.cos(TAU * v),
+            np.sin(TAU * v),
+            2 * (1 - u)
+        ]),
+    ).fade(0.5)
+
+    paraboloid = Surface(
+        lambda u, v: np.array([
+            np.cos(v)*u,
+            np.sin(v)*u,
+            u**2
+        ]),
+        v_range=(0,TAU),
+    ).fade(0.5)
+
+    para_hyp = Surface(
+        lambda u, v: np.array([
+            u,
+            v,
+            u**2-v**2
+        ]),
+        u_range=(-2,2),
+        v_range=(-2,2),
+    ).fade(0.5)
+
+    cone = Surface(
+        lambda u, v: np.array([
+            u*np.cos(v),
+            u*np.sin(v),
+            u
+        ]),
+        u_range=(-2,2),
+        v_range=(0,TAU),
+    )
+    sphere = Surface(
+        lambda u, v: np.array([
+            1.5*np.cos(u)*np.cos(v),
+            1.5*np.cos(u)*np.sin(v),
+            1.5*np.sin(u)
+        ]),
+        #Resolution of the surfaces
+        u_range=(-PI/2,PI/2),
+        v_range=(0,TAU),
+    )
+    axes = ThreeDAxes(x_range=(0, 5, 1), y_range=(0, 5, 1), z_range=(-1, 1, 0.5))
+    sphere.set_fill_by_value(axes=axes, colorscale=[(RED, -0.5), (YELLOW, 0), (GREEN, 0.5)], axis=2)
+
+    self.add(
+        axes_3d,surface
+    )
+    self.play(
+        Transform(surface, paraboloid)
+    )
+    self.begin_3dillusion_camera_rotation(rate=2)
+    self.wait(0.5)
+    self.play(
+        Transform(surface, para_hyp)
+    )
+    self.wait(0.5)
+    self.play(
+        Transform(surface, cone)
+    )
+    self.wait(0.5)
+    self.play(
+        Transform(surface, sphere)
+    )
+    self.wait(0.5)
